2a20474d5e4489423f8cfa28e73dae46fdf5f981
Nick Quinn <n.quinn@optoma.co.uk>
2018-04-04 08:16:50 +0100

    Sample

---
+++ ReactSampleApp/ClientApp/src/components/BlogEntry.js
@@ -0,0 +1,51 @@
+﻿import React, { Component } from 'react';
+
+export class BlogEntry extends Component {
+    displayName = BlogEntry.name
+
+    constructor() {
+        super();
+        this.state = { blogEntries: [], loading: true };
+
+        fetch("api/Blog/Article/test")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ blogEntries: data, loading: false });
+            });
+    }
+
+    static renderBlogEntries(blogEntries) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {blogEntries.map(be =>
+                        <tr key={be.id}>
+                            <td>{be.title}</td>
+                            <td>{be.published}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading blog entries...</em></p>
+            : BlogEntry.renderBlogEntries(this.state.blogEntries);
+
+        return (
+            <div>
+                <h1>Blog entries</h1>
+                {contents}
+            </div>
+        );
+    }
+
+}
---
+++ ReactSampleApp/Controllers/BlogController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class BlogController : Controller
+    {
+        [HttpGet("[action]/{refName}")]
+        public IEnumerable<BlogPost> Article(string refName)
+        {
+            List<BlogPost> ls = new List<BlogPost>();
+
+            if (refName == "test")
+            {
+                ls.Add(new BlogPost() { Id = 1, Title = "Test blog 1", Body = "This is the body", Published = DateTime.Now });
+            }
+            else
+            {
+                ls.Add(new BlogPost() { Id = 2, Title = "Test blog 2", Body = "This is the body 2", Published = DateTime.Now });
+            }
+
+            return ls;
+        }
+
+        public class BlogPost
+        {
+            public string Title { get; set; }
+            public string Body { get; set; }
+            public DateTime Published { get; set; }
+            public int Id { get; set; }
+        }
+    }
+}
